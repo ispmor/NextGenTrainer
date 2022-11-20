d7b3ff9f38b980a8ea5649ede7f13399589d33b4
Bartosz <puszkarski.bartosz@gmail.com>
2022-11-20 13:20:28 +0100

    initial UI for login and chooser activity

--- app/src/main/java/com/nextgentrainer/kotlin/data/models/User.kt
+++ app/src/main/java/com/nextgentrainer/kotlin/data/models/User.kt
@@ -1,11 +1,16 @@
 package com.nextgentrainer.kotlin.data.models
 
+import java.util.Date
+
 data class User(
     val login: String,
     val email: String,
     val password: String,
     val age: Int,
-    val weight: Double,
-    val height: Int,
-    val gender: String
+    val weight: Double?,
+    val height: Int?,
+    val gender: String?,
+    val plan: String,
+    val active: Boolean,
+    val lastPayment: Date?
 )
